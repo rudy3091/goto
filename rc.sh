146c9a9825793027b65f3d95fb9583b199c2e90e
rudy3091 <grayblack313@gmail.com>
2021-04-19 17:44:02 +0900

    Update shell script

--- rc.sh
+++ rc.sh
@@ -1,6 +1,14 @@
 function goto() {
 	case $1 in
-		"add") echo "$(go-to $@)";;
-		*) eval ". $(go-to $@)" ;;
+		"add") echo "$(go-to $@)" ;;
+		*)
+			cmd=$(go-to $@)
+			if [ $? -eq 1 ]; then
+				echo "$cmd"
+			else
+				echo "$cmd"
+				eval ". $cmd"
+			fi
+			;;
 	esac
 }
