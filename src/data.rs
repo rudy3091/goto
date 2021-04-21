d5d64ef2be7f276175b80e4c7b297b32bc0bcae4
rudy3091 <grayblack313@gmail.com>
2021-04-21 19:48:58 +0900

    Fix typo
    in success message

--- src/data.rs
+++ src/data.rs
@@ -70,7 +70,7 @@ pub fn write<'a>(path_to_add: &'a String) -> Result<(), Box<dyn Error>> {
     let path_to_add = PathBuf::from(path_to_add);
     if let Ok(path) = fs::canonicalize(path_to_add) {
         file.write_all(&mut format!("{}\n", path.display()).as_bytes())?;
-        println!("{}", message::success(format!("{} has added to list!", path.display()).as_str()));
+        println!("{}", message::success(format!("{} has been added to list!", path.display()).as_str()));
     } else {
         println!("{}", message::error("invalid path format"));
     }
